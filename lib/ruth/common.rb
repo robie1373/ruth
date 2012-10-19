be9bfb9118ae80c80d4fae147220622c20a4c06a
Robie <robie1373@gmail.com>
2012-10-19 17:47:12 -0400

    again

--- lib/ruth/common.rb
+++ lib/ruth/common.rb
@@ -1,7 +1,8 @@
+require 'tmpdir'
+
 module Common
   def Common.dot_ruth
     if ENV['TRAVIS'] == true
-      require 'tmpdir'
       File.join(Dir.tmpdir, ".ruth")
     else
       File.join(ENV['home'], ".ruth")
