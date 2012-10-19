83e1a54a0b371d720eb66f42d67d85da21ee77d6
Robie <robie1373@gmail.com>
2012-10-19 14:19:18 -0400

    cleaning up win32 specific references. Thank you Travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ SimpleCov.start
 module Ruth
   require 'rspec'
   #require 'turn'
-  require 'win32console'
+  require 'win32console' if RUBY_PLATFORM =~ /(win32|w32)/
   require 'bundler'
   #require 'terminal-notifier-guard'
 
