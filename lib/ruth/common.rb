7ac7f47e45642c89044300f402c5029984999d88
Robie Lutsey <robie1373@gmail.com>
2012-10-19 23:48:51 -0400

    changed common.dot_ruth to work on os x win32 and hopefully linux

--- lib/ruth/common.rb
+++ lib/ruth/common.rb
@@ -2,10 +2,10 @@ require 'tmpdir'
 
 module Common
   def Common.dot_ruth
-    if ENV['TRAVIS']
-      File.join(Dir.tmpdir, ".ruth")
-    else
+    if  RUBY_PLATFORM =~ /(win32|w32)/i
       File.join(ENV['home'], ".ruth")
+    else
+      File.join(ENV['HOME'], ".ruth")
     end
   end
 end
